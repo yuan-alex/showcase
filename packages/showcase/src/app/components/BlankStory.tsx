832e098e02b0ba07a72549857b7b23568dcc6c1a
Alex Yuan <alex@alexyuan.me>
2023-02-14 16:02:29 -0500

    feat: basic mobile support

--- packages/showcase/src/app/components/BlankStory.tsx
+++ packages/showcase/src/app/components/BlankStory.tsx
@@ -1,13 +1,13 @@
 export const BlankStory = () => {
   return (
-    <div className="md:m-20 md:rounded-xl md:border md:p-10 md:shadow">
-      <p className="mb-6 text-3xl font-semibold">👋 Welcome to Showcase.js</p>
-      <p className="text-lg">
+    <div className="p-3 md:m-20 md:rounded-xl md:border md:p-10 md:shadow">
+      <p className="mb-6 text-2xl md:text-3xl font-semibold">👋 Welcome to Showcase.js</p>
+      <p className="md:text-lg">
         Showcase.js is a React story visualizer and test framework. Using
         Showcase, you can build a storybook-like environment for your React
         components and perform visual regression tests using your own CI.
       </p>
-      <p className="mt-5 text-lg">
+      <p className="mt-5 md:text-lg">
         To get started, create a story file in your project. For example, if you
         have a component called <code>Button</code> in a file called{" "}
         <code>Button.tsx</code>, create a file called{" "}
@@ -27,7 +27,7 @@ export const Primary: ComponentStory<typeof Button> = () => <Button>Primary</But
 `}
         </code>
       </pre>
-      <p className="mt-5 text-lg">
+      <p className="mt-5 md:text-lg">
         For more information, check out the{" "}
         <a className="underline" href="https://github.com/yuan-alex/showcase">
           documentation
--- packages/showcase/src/app/components/Home.tsx
+++ packages/showcase/src/app/components/Home.tsx
@@ -4,8 +4,8 @@ import { Sidebar } from "./Sidebar.jsx";
 
 export function Home() {
   return (
-    <div className="flex h-screen w-screen flex-row divide-x">
-      <div className="w-80 flex-none bg-gray-50">
+    <div className="flex h-screen w-screen flex-col lg:flex-row divide-x">
+      <div className="lg:w-80 flex-none bg-gray-50">
         <Sidebar />
       </div>
       <div className="flex-grow">
